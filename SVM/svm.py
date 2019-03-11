ad9a035e6c445ed9faef509a311b0b0f949fdb8e
supr3m3 <anthonymaiho@gmail.com>
2019-03-10 19:37:58 -0700

    Added Datasets, SVM, and UDP

---
+++ SVM/svm.py
@@ -0,0 +1,32 @@
+import numpy as np
+import sv
+import os
+
+"""
+SVM monitors the rate of incoming packets and will classify if its good/bad
+Input and Dataset for SVM:
+Source IP, Dest IP, Packet Interval, and Protocl
+"""
+class SVM:
+    def train(self, data):
+        self.data = data
+        """
+    
+        """
+    def openFile(filename):
+        """
+
+        """
+    
+    def classify(filename):
+        """
+        Determine ratio
+        """
+
+if __name__ == "__main__":
+
+    while True:
+        """
+        Use tshark to compature data and store into a .csv file
+        """
+        prediction('file.csv')
