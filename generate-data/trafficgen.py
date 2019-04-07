6c435f32205ee95a671bd3416d7f99c80d9de79c
condrint <condrint@gmail.com>
2019-04-07 10:39:38 -0700

    added ttl to tshark

--- generate-data/trafficgen.py
+++ generate-data/trafficgen.py
@@ -4,7 +4,7 @@ import time
 
 """
 Traffic capture:
-sudo tshark -i any -a duration:10 -f 'icmp or udp or tcp' -T fields -E separator=, -e frame.time_delta_displayed -e ip.addr -e ip.proto > test.csv
+sudo tshark -i any -a duration:10 -f 'icmp or udp or tcp' -T fields -E separator=, -e frame.time_delta_displayed -e ip.addr -e ip.proto -e ip.ttl> test.csv
 """
 sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 bytes = random._urandom(1024)
@@ -13,7 +13,7 @@ port = 80
 sent = 0
 start =  time.time()
 
-print 'Attack initialized'
+print('Attack initialized')
 
 
 while(1): #ctrl+c to exit
